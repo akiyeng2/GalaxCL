4d84fe14582722f13b0f14d11eccc93945fee2d6
scrblnrd3 <amalan.iyengar@gmail.com>
2015-07-17 10:43:34 -0400

    Initial Commit

---
+++ GalaxCL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GalaxCL
+//
+//  Created by Amalan Iyengar on 7/17/15.
+//  Copyright (c) 2015 Amalan Iyengar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
